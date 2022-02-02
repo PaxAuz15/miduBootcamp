8c87596e582cdf787ce785a80d53ced127c9b787
Luis Auz <luisauz.geek@gmail.com>
2022-02-02 16:09:59 -0500

    1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Title = ({text}) => {
+  return <h1>{text}</h1>
+}
+
+const Button = ({option,func}) => {
+  return <button onClick={func}>{option}</button>
+}
+
+const DataRepresent = ({option,counterValue}) => {
+  return <p>{`${option} ${counterValue}`}</p>
+}
+
+const INITIAL_COUNTER_STATE = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+const App = () => {
+
+  const [counter, setCounter] = useState(INITIAL_COUNTER_STATE)
+
+  function handleClickGood(){
+    setCounter(preventData => {
+      return {
+        ...preventData,
+        good: preventData.good + 1
+      }
+    })
+  }
+
+  function handleClickNeutral(){
+    setCounter(preventData => {
+      return {
+        ...preventData,
+        neutral: preventData.neutral + 1
+      }
+    })
+  }
+
+  function handleClickBad(){
+    setCounter(preventData => {
+      return {
+        ...preventData,
+        bad: preventData.bad + 1
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title  text="give feedback"/>
+      <Button option="good" func={handleClickGood}/>
+      <Button option="neutral" func={handleClickNeutral}/>
+      <Button option="bad" func={handleClickBad}/>
+      <Title text="statistics"/>
+      <DataRepresent option="good" counterValue={counter.good}/>
+      <DataRepresent option="neutral" counterValue={counter.neutral}/>
+      <DataRepresent option="bad" counterValue={counter.bad}/>
     </div>
   );
 }
