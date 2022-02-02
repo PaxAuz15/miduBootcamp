053b1769df5a0d68ec91e16eb3ffe2976d051d30
Luis Auz <luisauz.geek@gmail.com>
2022-02-02 16:37:39 -0500

    1.9 unicafe step 4
    Display statistis only once feedback has been gathered

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,23 +1,29 @@
 import Title from "./Title"
 import DataRepresent from "./DataRepresent"
 
-const Statistics = ({counter,all,average,positive}) => {
+const Statistics = ({ counter, all, average, positive }) => {
   return (
     <>
       <Title text="statistics" />
-      <DataRepresent option="good" counterValue={counter.good} />
-      <DataRepresent option="neutral" counterValue={counter.neutral} />
-      <DataRepresent option="bad" counterValue={counter.bad} />
-      <DataRepresent option="all" counterValue={all} />
       {all === 0 ? (
-        <>
-          <DataRepresent option="average" counterValue={0} />
-          <DataRepresent option="positive" counterValue="0%" />
-        </>
+        <p>No feedback given</p>
       ) : (
         <>
-          <DataRepresent option="average" counterValue={average} />
-          <DataRepresent option="positive" counterValue={positive} />
+          <DataRepresent option="good" counterValue={counter.good} />
+          <DataRepresent option="neutral" counterValue={counter.neutral} />
+          <DataRepresent option="bad" counterValue={counter.bad} />
+          <DataRepresent option="all" counterValue={all} />
+          {all === 0 ? (
+            <>
+              <DataRepresent option="average" counterValue={0} />
+              <DataRepresent option="positive" counterValue="0%" />
+            </>
+          ) : (
+            <>
+              <DataRepresent option="average" counterValue={average} />
+              <DataRepresent option="positive" counterValue={positive} />
+            </>
+          )}
         </>
       )}
     </>
