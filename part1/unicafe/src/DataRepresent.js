e6507554f3bade8a805e25aa582769f237349917
Luis Auz <luisauz.geek@gmail.com>
2022-02-02 16:48:50 -0500

    1.10: unicage step5 && 1.11: step6

--- part1/unicafe/src/DataRepresent.js
+++ part1/unicafe/src/DataRepresent.js
@@ -1,5 +1,6 @@
 const DataRepresent = ({ option, counterValue }) => {
-  return <p>{`${option} ${counterValue}`}</p>
+//   return <p>{`${option} ${counterValue}`}</p>
+    return <><tr><td>{option}</td><td>{counterValue}</td></tr></> 
 }
 
 export default DataRepresent
