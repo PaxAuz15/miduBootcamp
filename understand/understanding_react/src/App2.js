40c68dbc5a3c2b126472294585d6f7f422a25274
Luis Auz <luisauz.geek@gmail.com>
2022-02-02 11:16:07 -0500

    better way to work with complex state
    url: https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#complex-state

---
+++ understand/understanding_react/src/App2.js
@@ -0,0 +1,19 @@
+import "./App.css"
+import { useState } from "react"
+
+// Correct way: Separate States because is more atomic.
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
+    </div>
+  )
+}
+
+export default App
