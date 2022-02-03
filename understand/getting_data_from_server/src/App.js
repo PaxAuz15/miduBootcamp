9d491172fd3125b51cad593cb63688a7bf8e45db
Luis Auz <luisauz.geek@gmail.com>
2022-02-03 17:06:24 -0500

    feat: setting notes like posts on jsonplaceholder

--- understand/getting_data_from_server/src/App.js
+++ understand/getting_data_from_server/src/App.js
@@ -1,49 +1,41 @@
 import { useState } from "react"
 
-const Note = ({content,date}) => {
-  return <li>
-    <p>{content}</p>
-    <time>{date}</time>
-  </li>
+const Note = ({ title, body }) => {
+  return (
+    <li>
+      <p>{title}</p>
+      <small>{body}</small>
+    </li>
+  )
 }
 
 const App = (props) => {
   const [notes, setNote] = useState(props.notes)
   const [newNote, setNewNote] = useState("")
-  const [showAll, setShowAll] = useState(true)
 
   function handleChange(event) {
     setNewNote(event.target.value)
   }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault()
     const newNoteToAdd = {
       id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random < 0.5
+      title: newNote,
+      body: newNote
     }
-    setNote([...notes,newNoteToAdd])
+    setNote([...notes, newNoteToAdd])
     setNewNote("")
   }
-
-  function handleShowAll(){
-    setShowAll(!showAll)
-  }
-
 
   return (
     <div className="App">
       <h1>Notes</h1>
-      <button onClick={handleShowAll}>{showAll?"Show only important":"Show All"}</button>
-      <ul>
-        {showAll?notes.map((note) => {
-          return <Note key={note.id} {...note}/>
-        }):notes.filter(note=>note.important ===true).map((note) => {
-          return <Note key={note.id} {...note}/>
+      <ol>
+        {notes.map((note) => {
+          return <Note key={note.id} {...note} />
         })}
-      </ul>
+      </ol>
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} value={newNote}></input>
         <button>Add</button>
--- understand/getting_data_from_server/src/index.js
+++ understand/getting_data_from_server/src/index.js
@@ -5,23 +5,23 @@ import reportWebVitals from './reportWebVitals';
 
 const notes = [
   {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
 ]
 
 ReactDOM.render(
