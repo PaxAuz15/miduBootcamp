cf7a5f7b12c5ae68ed0362e123f570811e6807c6
Luis Auz <luisauz.geek@gmail.com>
2022-02-03 15:51:37 -0500

    feat: note app without form

--- understand/forms/src/App.js
+++ understand/forms/src/App.js
@@ -1,6 +1,7 @@
 import { Note } from "./Note"
+import { useState } from "react"
 
-const Notes = ({notes}) => {
+const Notes = ({ notes }) => {
   return notes.map((note) => {
     return (
       <Note
@@ -13,13 +14,35 @@ const Notes = ({notes}) => {
   })
 }
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNote] = useState(props.notes)
+  const [newNote, setNewNote] = useState("")
+
+  function handleChange(event) {
+    setNewNote(event.target.value)
+  }
+
+  function handleClick(event){
+    const newNoteToAdd = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+    setNote([...notes,newNoteToAdd])
+    setNewNote("")
+  }
+
   return (
     <div className="App">
-      <h1>Bootcamp Class #4</h1>
+      <h1>Notes</h1>
       <ol>
-        <Notes notes={notes}/>
+        <Notes notes={notes} />
       </ol>
+      <div>
+        <input type="text" onChange={handleChange} value={newNote}/>
+        <button onClick={handleClick}>Create Note</button>
+      </div>
     </div>
   )
 }
