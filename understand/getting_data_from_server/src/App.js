05435c2ff490dddfa42a5e7790b1ccd71a5b9e14
Luis Auz <luisauz.geek@gmail.com>
2022-02-03 16:54:02 -0500

    feat: note app created

--- understand/getting_data_from_server/src/App.js
+++ understand/getting_data_from_server/src/App.js
@@ -1,9 +1,55 @@
-const App = () => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { useState } from "react"
+
+const Note = ({content,date}) => {
+  return <li>
+    <p>{content}</p>
+    <time>{date}</time>
+  </li>
 }
 
-export default App;
+const App = (props) => {
+  const [notes, setNote] = useState(props.notes)
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
+
+  function handleChange(event) {
+    setNewNote(event.target.value)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    const newNoteToAdd = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random < 0.5
+    }
+    setNote([...notes,newNoteToAdd])
+    setNewNote("")
+  }
+
+  function handleShowAll(){
+    setShowAll(!showAll)
+  }
+
+
+  return (
+    <div className="App">
+      <h1>Notes</h1>
+      <button onClick={handleShowAll}>{showAll?"Show only important":"Show All"}</button>
+      <ul>
+        {showAll?notes.map((note) => {
+          return <Note key={note.id} {...note}/>
+        }):notes.filter(note=>note.important ===true).map((note) => {
+          return <Note key={note.id} {...note}/>
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={newNote}></input>
+        <button>Add</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
