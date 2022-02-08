0dce2aaa50553b6384866808c2013f18cfdb0fa3
Luis Auz <luisauz.geek@gmail.com>
2022-02-08 08:42:12 -0500

    feat: using express to send data by route

--- part3/index.js
+++ part3/index.js
@@ -1,32 +1,43 @@
-const http = require("http")
+// const http = require("http")
+const express = require('express')
+const app = express()
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
     important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
     important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
     important: true,
   },
 ]
 
-const app = http.createServer((request, response) => {
-  console.log({ request })
-  response.writeHead(200, { "Content-Type": "application/json" })
-  response.end(JSON.stringify(notes))
+// const app = http.createServer((request, response) => {
+//   console.log({ request })
+//   response.writeHead(200, { "Content-Type": "application/json" })
+//   response.end(JSON.stringify(notes))
+// })
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 const PORT = 3002
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
