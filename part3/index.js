23f4d3482a9e331cfae05cfcfc00beed8b0365a3
Luis Auz <luisauz.geek@gmail.com>
2022-02-08 07:35:17 -0500

    feat: autoindenti with save

--- part3/index.js
+++ part3/index.js
@@ -1,28 +1,29 @@
-const http = require('http')
+const http = require("http")
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+]
 
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
+  console.log({ request })
+  response.writeHead(200, { "Content-Type": "application/json" })
   response.end(JSON.stringify(notes))
 })
 
